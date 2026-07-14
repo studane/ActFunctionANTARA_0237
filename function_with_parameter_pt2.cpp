d76635247df703ce98087d219558667b8eb4c051
studane <zhidaneprabowoguzlend@gmail.com>
2026-07-14 10:41:13 +0700

    membuat program utama

--- function_with_parameter_pt2.cpp
+++ function_with_parameter_pt2.cpp
@@ -13,3 +13,16 @@ string CekStatusBerparameter(float a){
         return "Tidak Lulus";
     }
 }
+
+int main()
+{
+    float Ujian1, Ujian2;
+
+    cout << "Masukkan Nilai Ujian 1 : ";
+    cin >> Ujian1;
+    cout << "Masukkan Nilai Ujian 2 : ";
+    cin >> Ujian2;
+
+    cout << "Status Kelulusan : "
+    << CekStatusBerparameter(HitungRerataBerparameter(Ujian1, Ujian2));
+}
