a87b0d9c0d9519c8b14319108fa4c4a9f05eb918
studane <zhidaneprabowoguzlend@gmail.com>
2026-07-14 10:31:11 +0700

    membuat program utama

--- function_with_parameter.cpp
+++ function_with_parameter.cpp
@@ -25,3 +25,10 @@ void output()
 {
     cout << "Luasnya : " << luasPersegi(panjang, lebar) << endl;
 }
+
+int main()
+{
+    input();
+    output();
+    cout << "Kelilingnya : " << kelilingPersegi(panjang, lebar);
+}
